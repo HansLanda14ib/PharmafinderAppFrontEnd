54269a38565cb8adff974998134a943bf2461509
Hansa Landa <bibzazne@gmail.com>
2023-06-04 00:36:53 +0100

    last commit

--- src/Services/auth.service.jsx
+++ src/Services/auth.service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/v1/auth/";
+const API_URL = "https://pharmafinder.up.railway.app/api/v1/auth/";
 
 const register = (role,
                   email,
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const apiUrl = "http://localhost:8080/api/v1";
+const apiUrl = "https://pharmafinder.up.railway.app/api/v1";
 export default apiUrl;
