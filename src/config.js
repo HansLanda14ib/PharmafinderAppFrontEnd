b4bce9c20dcd34c0aa2da46beebbc8478220c8e2
Hansa Landa <bibzazne@gmail.com>
2023-06-04 00:53:28 +0100

    cors-anywhere-production-6dbe.up.railway.app

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const apiUrl = "https://pharmafinder.up.railway.app/api/v1";
+const apiUrl = "cors-anywhere-production-6dbe.up.railway.apphttps://pharmafinder.up.railway.app/api/v1";
 export default apiUrl;
